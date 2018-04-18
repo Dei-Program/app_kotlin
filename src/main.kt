eb304be9ec5616e6234185b34ceb340af13b0bf2
Dei-Program <david.condor@epn.edu.ec>
2018-04-18 10:34:26 -0500

    jaja

---
+++ src/main.kt
@@ -0,0 +1,18 @@
+import javax.print.attribute.IntegerSyntax
+
+class  Persona{
+    lateinit var edadActual:Integer
+//    constructor(edad:Integer){
+//        edadActual =edad}
+constructor(edad:Integer, nombre:String){
+    iniciar(dad)
+}
+
+
+}
+
+constru
+
+
+
+}
